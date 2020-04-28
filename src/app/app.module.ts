03e74373c8d17af3d9d2fcd9582f290272fb2842
SandovalJr <edubizzle7@gmail.com>
2020-04-28 11:57:34 -0500

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Importamos clase
+// ctrl + space = help
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // aqui van los componentes (para que angular vea los componentes que puede utlizar)
+  declarations: [AppComponent, HeaderComponent, FooterComponent , BodyComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header', // selector: indica nombre de componente
+  templateUrl: './header.component.html',
+})
+
+
+export class HeaderComponent {
+
+
+}
